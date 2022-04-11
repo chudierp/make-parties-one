46c7f9b2d89385904ddc59c5b338ef1dcdb501e6
Chudier <chudierp@gmail.com>
2022-04-11 15:56:45 -0700

    proper separation of concerns

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const methodOverride = require('method-override')
 const { engine } = require('express-handlebars');
 
+
 // INITIALIZE BODY-PARSER AND ADD IT TO APP
 const bodyParser = require('body-parser');
 
@@ -14,6 +15,8 @@ const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-acce
 
 const app = express()
 // const db = require('db');
+
+require('./controllers/events')(app, models);
 
 app.use(methodOverride('_method'))
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -31,69 +34,12 @@ var events = [
 ]
   
 // INDEX
-app.get('/', (req, res) => {
-    models.Event.findAll({ order: [['createdAt', 'DESC']] }).then(events => {
-      res.render('events-index', { events: events });
-    })
-  })
+// app.get('/', (req, res) => {
+//     models.Event.findAll({ order: [['createdAt', 'DESC']] }).then(events => {
+//       res.render('events-index', { events: events });
+//     })
+//   })
 
-// CREATE
-app.post('/events', (req, res) => {
-    models.Event.create(req.body).then(event => {
-      res.redirect(`/events/${event.id}`);
-    }).catch((err) => {
-      console.log(err)
-    });
-  })
-
-// NEW
-app.get('/events/new', (req, res) => {
-    res.render('events-new', {});
-})
-
-// SHOW
-app.get('/events/:id', (req, res) => {
-    // Search for the event by its id that was passed in via req.params
-    models.Event.findByPk(req.params.id).then((event) => {
-      // If the id is for a valid event, show it
-      res.render('events-show', { event: event })
-    }).catch((err) => {
-      // if they id was for an event not in our db, log an error
-      console.log(err.message);
-    })
-})
-
-// EDIT
-app.get('/events/:id/edit', (req, res) => {
-    models.Event.findByPk(req.params.id).then((event) => {
-      res.render('events-edit', { event: event });
-    }).catch((err) => {
-      console.log(err.message);
-    })
-});
-
-// UPDATE
-app.put('/events/:id', (req, res) => {
-    models.Event.findByPk(req.params.id).then(event => {
-      event.update(req.body).then(event => {
-        res.redirect(`/events/${req.params.id}`);
-      }).catch((err) => {
-        console.log(err);
-      });
-    }).catch((err) => {
-      console.log(err);
-    });
-});
-
-// DELETE
-app.delete('/events/:id', (req, res) => {
-    models.Event.findByPk(req.params.id).then(event => {
-      event.destroy();
-      res.redirect(`/`);
-    }).catch((err) => {
-      console.log(err);
-    });
-  })
 
 
 // Choose a port to listen on   
