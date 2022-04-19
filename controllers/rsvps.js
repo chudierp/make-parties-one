82460af91279a06172b0da6ccfe2f28a1c66330e
Chudier <chudierp@gmail.com>
2022-04-19 13:35:15 -0700

    rsvp not associated to event error

--- controllers/rsvps.js
+++ controllers/rsvps.js
@@ -6,15 +6,15 @@ module.exports = (app, models) => {
       });
     });
   
-      // CREATE
+// CREATE
     app.post('/events/:eventId/rsvps', (req, res) => {
         req.body.EventId = req.params.eventId;
         models.Rsvp.create(req.body).then(rsvp => {
-        res.redirect(`/events/${req.params.eventId}`);
+            res.redirect(`/events/${req.params.eventId}`);
         }).catch((err) => {
             console.log(err)
         });
-    });ssaddxa
+    });
     
     // DESTROY  
  }
